085483c977081395e7e94a26f00624a60f21b696
Sachin KM <hiscan99@gmail.com>
2025-09-30 16:51:32 +0200

    Refactor code structure for improved readability and maintainability

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.hagsr11jea8"
+    "revision": "0.2b5v506uuao"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
